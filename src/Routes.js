d7f9ba196418f42f271e54467bb7128fa9bf38bb
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-06 20:58:41 +0530

    frontend

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signin from "./user/Signin";
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import PrivateRoute from "./auth/PrivateRoute";
+import AdminRoute from "./auth/AdminRoute";
+import Dashboard from "./user/UserDashboard";
+import AdminDashboard from "./user/AdminDashboard";
+
+function Routes() {
+  console.log(window.location.href);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
+        <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
---
+++ src/core/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Home = () => (
+    <Layout
+      title="Home"
+      description="Node React E-commerce App"  
+    >
+      ....
+    </Layout>
+  );
+
+
+export default Home;
