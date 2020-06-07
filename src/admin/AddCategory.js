34562de45df6c8eb03270a652f7dadf9ffa5c252
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-07 09:18:57 +0530

    adding apiAdmin comp

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth/index";
 import { Link } from "react-router-dom";
+import { createCategory } from './apiAdmin';
 
 const AddCategory = () => {
   const [name, setName] = useState("");
@@ -21,7 +22,37 @@ const AddCategory = () => {
     setError("");
     setSuccess(false);
     //make request to api
+    createCategory(user._id, token, {name})
+    .then(data =>{
+        if(data.error){
+            setError(true)
+        }else{
+            setError('')
+            setSuccess(true)
+        }
+    })
   };
+
+
+  const showSuccess = () =>{
+      if(success){
+      return <h3 className="text-success">{name} is created</h3>
+      }
+  };
+
+   const showError = () => {
+     if (error) {
+       return <h3 className="text-danger">Category should be unique</h3>;
+     }
+   };
+
+   const goBack = () =>(
+       <div className="nt-5">
+        <link to="/admin/dashboard" className="text-warning">
+            Back to Dashboard
+        </link>
+       </div>
+   );
 
   const newCategoryForm = () => (
     <form onSubmit="clickSubmit">
@@ -33,6 +64,7 @@ const AddCategory = () => {
           vlaue={name}
           onChange={handleChange}
           autoFocus
+          required
         />
       </div>
       <button className="btn btn-outline-primary">Create Category</button>
@@ -42,10 +74,15 @@ const AddCategory = () => {
   return (
     <Layout
       title="Add a new category"
-      description={`G'day ${name}, ready to add a new category`}
+      description={`G'day ${user.name}, ready to add a new category`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+        <div className="col-md-8 offset-md-2">
+            {showSuccess()}
+            {showError()}
+            {newCategoryForm()}
+            {goBack()}
+            </div>
       </div>
     </Layout>
   );
